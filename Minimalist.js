f09586baa90ea6e7096d70e55be2405b566a21d6
areH <98583344+paratmr@users.noreply.github.com>
2024-01-06 15:20:42 +0800

    Update Minimalist.js

--- Minimalist.js
+++ Minimalist.js
@@ -17,13 +17,7 @@
     "rawColors": {
         "BRAND_260": "#9b9b9b",
         "BRAND_360": "#9b9b9b",
-        "BRAND_500": "#757575",
-        "BRAND_560": "#9b9b9b",
-        "PRIMARY_400": "#9b9b9b",
-        "PRIMARY_600": "#2a2a2a",
-        "PRIMARY_660": "#2a2a2a",
-        "RED_400": "#9b9b9b",
-        "YELLOW_300": "#757575"
+        "BRAND_500": "#757575"
     },
     "background": {
         "blur": 0,
