3556e251a300a1e910af2cc74cbab7b07427581e
areH <98583344+paratmr@users.noreply.github.com>
2024-01-06 15:03:57 +0800

    Create Minimalist.js

---
+++ Minimalist.js
@@ -0,0 +1,29 @@
+{
+    "name": "最小限 Dimmed Greyish",
+    "description": "A theme made by ayrisj",
+    "authors": [
+        {
+            "name": "ayrisj",
+            "id": "735310025064448030"
+        }
+    ],
+    "semanticColors": {
+        "BACKGROUND_ACCENT": ["#757575"]
+    },
+    "rawColors": {
+        "BRAND_260": "#9b9b9b",
+        "BRAND_360": "#9b9b9b",
+        "BRAND_500": "#757575",
+        "BRAND_560": "#9b9b9b",
+        "PRIMARY_400": "#9b9b9b",
+        "PRIMARY_600": "#2a2a2a",
+        "PRIMARY_660": "#2a2a2a",
+        "RED_400": "#9b9b9b",
+        "YELLOW_300": "#757575"
+    },
+    "background": {
+        "blur": 0,
+        "alpha": 0.68
+    },
+    "spec": 2
+    
